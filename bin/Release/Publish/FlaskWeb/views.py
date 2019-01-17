e8ab57d5229230ebcb5a7fcda0ff9c1df343c1cd
liujia <214022568@qq.com>
2019-01-17 15:21:58 +0800

    init

---
+++ bin/Release/Publish/FlaskWeb/views.py
@@ -0,0 +1,43 @@
+"""
+Routes and views for the flask application.
+"""
+import sys
+sys.path.append(r"D:\python\FlaskWeb\FlaskWeb")
+import utils.httpClientHelper
+from datetime import datetime
+from flask import render_template
+from FlaskWeb import app
+
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """呈现主页  ."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+        neirong='hellow python' #utils.httpClientHelper.httpPostMethod("http://172.16.1.122:63310/api/RealtyFiles/GetRealtyFiles","{'areaCode':'B024','realtyid':2279071}")#
+        
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
